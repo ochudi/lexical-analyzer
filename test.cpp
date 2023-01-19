915749898986b9a6bd070c84cda58353e8a5f430
ochudi <chukwudi.ofoma@pau.edu.ng>
2023-01-19 12:13:47 +0100

    Assignment set up

---
+++ test.cpp
@@ -0,0 +1,11 @@
+// A code fragment that stores the result of multiplication of 2 numbers
+#include <iostream>
+using namespace std;
+main()
+{
+	int b = 2;
+	// v = 5;
+	float c = 2.44;
+	int d = b * c;
+	string myname = "Taiwo Amoo";
+}
